076ca80fecaf37a50cee8ad58f9827a169c0563b
Tai Naoyuki <tai3424@ssk-kan.co.jp>
2026-02-13 20:59:21 +0900

    日本語ロケールを設定しました。提出期日の英語表示に対応しました。

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
@@ -8,9 +8,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import { apiInterceptor } from './core/api.interceptor';
 
+import { registerLocaleData } from '@angular/common';
+import localeJa from '@angular/common/locales/ja';
+
+registerLocaleData(localeJa);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideHttpClient(withInterceptors([apiInterceptor])), provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'ja-JP' },
   ],
 };
